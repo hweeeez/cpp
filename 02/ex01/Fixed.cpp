614f313c16df6f9cac19527a041b202a53c3c778
hweeeez <huiiyihuiyi@gmail.com>
2025-07-27 01:05:01 +0800

    fixed

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -1,4 +1,5 @@
 #include "Fixed.hpp"
+#include <cmath>
 
 Fixed::Fixed() : value(0)
 {
@@ -20,7 +21,7 @@ Fixed::Fixed(const int val)
 Fixed::Fixed(const float val)
 {
     std::cout << "Float Constructor " << value << '\n';
-    value = val * (float)(1 << frac);
+    value = roundf(val * (float)(1 << frac));
 }
 
 Fixed::~Fixed()
@@ -52,7 +53,7 @@ int Fixed::getRawBits(void) const
 
 int Fixed::toInt(void) const
 {
-    return this->value << frac;
+    return this->value >> frac;
 }
 
 float Fixed::toFloat(void) const
--- 02/ex02/Fixed.cpp
+++ 02/ex02/Fixed.cpp
@@ -3,35 +3,35 @@
 
 Fixed::Fixed() : value(0)
 {
-    std::cout << "Default Constructor" << '\n';
+    //std::cout << "Default Constructor" << '\n';
 }
 
 Fixed::Fixed(const Fixed &fixed)
 {
-    std::cout << "Copy Constructor" << '\n';
+    //std::cout << "Copy Constructor" << '\n';
     *this = fixed;
 }
 
 Fixed::Fixed(const int val)
 {
     this->value = val << frac;
-    std::cout << "Int Constructor " << value << '\n';
+    //std::cout << "Int Constructor " << value << '\n';
 }
 
 Fixed::Fixed(const float val)
 {
-    std::cout << "Float Constructor " << value << '\n';
+    //std::cout << "Float Constructor " << value << '\n';
     value = roundf(val * (float)(1 << frac));
 }
 
 Fixed::~Fixed()
 {
-    std::cout << "Destructor" << '\n';
+    //std::cout << "Destructor" << '\n';
 }
 
 Fixed& Fixed::operator=(const Fixed &fixed)
 {
-    std::cout << "Copy Assignment Operator" << '\n';
+    //std::cout << "Copy Assignment Operator" << '\n';
     if (this != &fixed)
     {
         this->setRawBits(fixed.getRawBits());
@@ -41,19 +41,19 @@ Fixed& Fixed::operator=(const Fixed &fixed)
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << "Setting raw bits" << '\n';
+    //std::cout << "Setting raw bits" << '\n';
     value = raw;
 }
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "Getting raw bits" << '\n';
+    //std::cout << "Getting raw bits" << '\n';
     return value;
 }
 
 int Fixed::toInt(void) const
 {
-    return this->value << frac;
+    return this->value >> frac;
 }
 
 float Fixed::toFloat(void) const
