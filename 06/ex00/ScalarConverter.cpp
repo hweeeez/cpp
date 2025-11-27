9df2742d1b8e4db14b47edd366f91c0400c70978
hweeeez <huiiyihuiyi@gmail.com>
2025-11-27 23:12:48 +0800

    -

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -25,8 +25,15 @@ ScalarConverter::~ScalarConverter()
 void ScalarConverter::convert(std::string input)
 {
     //char int float double
-if (input.find_first_not_of("0123456789") == std::string::npos) {
-    std::cout << "string '" << s << "' contains only digits\n";
+    size_t e = input.find_first_not_of("0123456789");
+    if (e == std::string::npos) {
+        std::cout << "string '" << input << "' contains only digits\n";
+    }
+    if (e > 0 && e != std::string::npos) {
+        if (input.find_last_of("f") == input.length() - 1)
+        {
+            std::cout << "float" << '\n';
+        }
 }
 
 }
